94f7748cf962419ffb335ccc5d12df15302e7160
Luke Jimenez <lucas.s.jimenez@gmail.com>
2025-11-11 13:43:02 -0500

    Added test case for second derivative rbf fd

--- Test_Cases/fd_testcase_d2.py
+++ Test_Cases/fd_testcase_d2.py
@@ -18,14 +18,17 @@ start_time = time.time()
 def true_func(x):
     return np.sin(2*np.pi * x)
 
-def true_func_dx(x):
-    return 2*np.pi * np.cos(2*np.pi * x)
+def true_func_dxx(x):
+    return -4*(np.pi*np.pi) * np.sin(2*np.pi * x)
 
 def phs(r):
     return (abs(r) ** (degree))
 
 def deriv_phs(r):
     return degree * (np.abs(r) ** (degree - 1))
+
+def deriv2_phs(r):
+    return degree * (degree - 1) * (np.abs(r) ** (degree - 2))
 
 def list_to_matrix(list):
       return np.matrix(np.array(list))
@@ -46,102 +49,86 @@ def knn_matrix_creator(xvals,n_knn):
     distances = []
   return matrix, indices
 
-### Base Functions
-
-def poly_terms_and_dt(var1, t, poly_degree):
-    """
-    Returns:
-        terms: list of polynomial terms evaluated at (x, t)
-        dterms_dt: list of derivatives of those terms with respect to t
-    """
-    terms = []
-    dterms_dt = []
-    # Generate all monomials: x^i * t^j where i + j <= poly_degree
-    for i in range(poly_degree + 1):
-        for j in range(poly_degree + 1 - i):
-            term = (var1 ** i) * (t ** j)
-            # Derivative w.r.t t: j * x^i * t^(j-1) (if j > 0), else 0
-            if j == 0:
-                dterm_dt = 0
-            else:
-                dterm_dt = j * (var1 ** i) * (t ** (j - 1))
-                print("i: " + str(i) + " j: " + str(j))
-            terms.append(term)
-            dterms_dt.append(dterm_dt)
-    return terms, dterms_dt
 
 # Example usage:
 poly_degree = 3
 
-def local_rbf_fd(xvals = xvals, poly_degree = poly_degree):
+def local_rbf_fd_d2(xvals = xvals, poly_degree = poly_degree):
+    """
+    Calculates the SECOND derivative (D2) matrix.
+    Written in the same style as the original local_rbf_fd.
+    """
+    num_knn = (2*poly_degree) + 1  # creates x values into a np.array
     
-    num_knn = (2*poly_degree) + 1
-   
+    # Kept your original (inefficient) way of calling this twice
     knn_matrix = knn_matrix_creator(xvals,num_knn)[0]
     indices = knn_matrix_creator(xvals,num_knn)[1]
     length = len(xvals)
     final_matrix = np.zeros(len(xvals)**2).reshape(len(xvals),len(xvals))
 
-
+    # Kept these unused variables
     X, Y = np.meshgrid(xvals, xvals)
     final_matrices = []
     local_diff_matrix =[]
     center = 0
 
     for i in range(length):
-      knn = []
-      for j in range(num_knn):
-        index = indices[i][j]
-        knn.append(xvals[index])
+        knn = []
+        for j in range(num_knn):
+            index = indices[i][j]
+            knn.append(xvals[index])
 
-      X, Y = np.meshgrid(knn, knn)
-      r = X-Y
-      phs_matrix= np.ones(len(r))
+        X, Y = np.meshgrid(knn, knn)
+        r = X-Y
+        phs_matrix= np.ones(len(r))
 
-    #Creates the matrix of distances between the sample points
-      for z in range(0,len(r)):
-          row = phs(r[z])
-          phs_matrix = np.vstack((phs_matrix,row))
-      phs_matrix = phs_matrix[1:]
-
-      sol_vec = np.array(deriv_phs(r[0]))
-     #Creates the matrix of added polynomial terms
-      poly_matrix = []
-      for z in range(len(knn)):
-        row = []
-        row.append(1)
-        for k in range(1,poly_degree+1):
-            row.append(knn[z]**(k))
-        poly_matrix.append(row)
-      phs_matrix = list_to_matrix(phs_matrix)
-      poly_matrix = list_to_matrix(poly_matrix)
-      poly_matrix_t = poly_matrix.getT() #getT gets the transpose of the matrix
-      zeroes = np.matrix(np.array([0]*(poly_degree+1)**2).reshape(poly_degree+1,poly_degree+1))
+        #Creates the matrix of distances between the sample points
+        for z in range(0,len(r)):
+            row = phs(r[z])
+            phs_matrix = np.vstack((phs_matrix,row))
+        phs_matrix = phs_matrix[1:]
 
 
-      top_half_A = np.hstack((phs_matrix.getA(),poly_matrix.getA())) #getA makes the matrix an array which allows us to stack it
-      bottom_half_A = np.hstack((poly_matrix_t.getA(), zeroes.getA()))
-      A_matrix = np.matrix(np.vstack((top_half_A,bottom_half_A)))
 
-      poly_deriv_vec = []
-      center_node_x = knn[0] # The center of the stencil
+        sol_vec = np.array(deriv2_phs(r[0]))
+
+
+        #Creates the matrix of added polynomial terms
+        poly_matrix = []
+        for z in range(len(knn)):
+            row = []
+            row.append(1)
+            for k in range(1,poly_degree+1):
+                row.append(knn[z]**(k))
+            poly_matrix.append(row)
+        
+
+        phs_matrix = list_to_matrix(phs_matrix)
+        poly_matrix = list_to_matrix(poly_matrix)
+        poly_matrix_t = poly_matrix.getT() #getT gets the transpose of the matrix
+        zeroes = np.matrix(np.array([0]*(poly_degree+1)**2).reshape(poly_degree+1,poly_degree+1))
+        
+        top_half_A = np.hstack((phs_matrix.getA(),poly_matrix.getA()))
+        bottom_half_A = np.hstack((poly_matrix_t.getA(), zeroes.getA()))
+        A_matrix = np.matrix(np.vstack((top_half_A,bottom_half_A)))
+
+        poly_deriv_vec = []
+        center_node_x = knn[0] # The center of the stencil
     
-      for k in range(poly_degree + 1):
-        if k == 0:
-            poly_deriv_vec.append(0.0)  # d(1)/dx = 0
-        else:
-            # d(x^k)/dx = k * x^(k-1)
-            poly_deriv_vec.append(k * (center_node_x ** (k - 1)))
-            
-      # Append the correct derivative vector
-      sol_vec = np.append(sol_vec, poly_deriv_vec)
+        for k in range(poly_degree + 1):
+            if k == 0 or k == 1:
+                poly_deriv_vec.append(0.0)  
+            else:
 
-      finite_weights = np.linalg.solve(A_matrix, sol_vec)
-      final_matrix[i][indices[i]] = finite_weights[0:num_knn]
-    d1_matrix = final_matrix
-    u = true_func(xvals)
-    u_dx_approx = d1_matrix @ u
-    return u_dx_approx
+                poly_deriv_vec.append(k * (k - 1) * (center_node_x ** (k - 2)))
+
+                
+        sol_vec = np.append(sol_vec, poly_deriv_vec)
+
+        finite_weights = np.linalg.solve(A_matrix, sol_vec)
+        final_matrix[i][indices[i]] = finite_weights[0:num_knn]
+    return final_matrix
+
 
 # Graphs The Derivative Approximation vs The True Derivative
 
@@ -172,9 +159,10 @@ for k in range(1,5): # Iterates over polynomial degrees
    for j in range(1,21): # Iterates over number of sample points
       num_pts = 30 + 10*j
       xvals = np.linspace(start, stop, num=num_pts)  # creates x values into a np.array
-      true_vals = true_func_dx(xvals)
-      est_deriv = local_rbf_fd(xvals, (k*2)+1)
-      error = np.log(np.max(np.abs(est_deriv - true_vals)))
+      true_d2_vals = true_func_dxx(xvals)
+      true_vals = true_func(xvals)
+      est_deriv = local_rbf_fd_d2(xvals, (k*2)+1) @ true_vals
+      error = np.log(np.max(np.abs(est_deriv - true_d2_vals)))
       if k == 1:
          poly_degree_3_vec.append(error)
          xvals_graph = np.append(xvals_graph, num_pts)
@@ -196,15 +184,5 @@ plt.ylabel('Base 10 Log of Max Error')
 plt.title('Base 10 Log of Max Error vs. Base 10 Log of n')
 plt.grid(False)
 plt.legend()
-plt.savefig(r"c:\Users\lucas\RBF_Research\RBF_Research\Test_Cases\deriv_error_vs_n.png", dpi=300, bbox_inches="tight")
+plt.savefig(r"c:\Users\lucas\RBF_Research\RBF_Research\Test_Cases\2nd_deriv_error_vs_n.png", dpi=300, bbox_inches="tight")
 plt.show()
-
-
-# Do another example with the D2 directly calculated and D1*D1
-
-
-
-    
-
-
-         
